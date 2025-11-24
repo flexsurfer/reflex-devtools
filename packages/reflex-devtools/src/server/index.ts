8e4c82050b207183fa54be93fda0784d848874e6
flexsurfer <flexsurfer@pm.me>
2025-11-24 14:26:20 +0100

    Update version to 0.1.12 and increase JSON body size limit in DevtoolsServer middleware

--- packages/reflex-devtools/src/server/index.ts
+++ packages/reflex-devtools/src/server/index.ts
@@ -53,7 +53,7 @@ export class DevtoolsServer {
 
   private setupMiddleware(): void {
     this.app.use(cors());
-    this.app.use(express.json());
+    this.app.use(express.json({ limit: '50mb' }));
     this.app.use(express.static(this.uiPath));
   }
 
